549044db2ba585c45a6a3119dfe28902587afc65
AmeerAlabi <ameeralabi7@gmail.com>
2026-02-06 23:40:43 +0100

    edit

--- src/config/env.ts
+++ src/config/env.ts
@@ -19,7 +19,7 @@ export const config = {
   },
   
   cors: {
-    frontendUrl: process.env.FRONTEND_URL || 'http://localhost:8080',
+    frontendUrl: process.env.FRONTEND_URL || 'https://www.zipfast.app/',
   },
   
   logging: {
